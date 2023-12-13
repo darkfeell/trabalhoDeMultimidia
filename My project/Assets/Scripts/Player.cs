a9e5e592863e4f2c393c5333fd3e540dabc27e70
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-12-13 17:41:23 -0300

    animations done, player movement and player jump

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -7,22 +7,29 @@ public class Player : MonoBehaviour
     [Header("Variables")]
     public float speed;
     public int health;
+    public float jumpForce;
     [Header("Booleans")]
     public bool isGrounded;
     [Header("Others")]
     public Animator anim;
     public Rigidbody2D rig;
+    public SpriteRenderer sprite;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        Jump();
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
         
     }
 
@@ -31,5 +38,26 @@ public class Player : MonoBehaviour
         float inputX = Input.GetAxisRaw("Horizontal");
 
         rig.velocity = new Vector2(inputX * speed, rig.velocity.y);
+
+        if (inputX > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (inputX < 0)
+        {
+            sprite.flipX = true;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            
+        }
     }
 }
