fbda027fae2b737dea8699705350b1d6f9cf2014
Miguel Chagas <miguel.chagas614@gmail.com>
2024-01-02 11:59:53 -0300

    Player Sound (Attack, Jump, Step) and refactoring

--- My project/Assets/Scripts/Player.cs
+++ My project/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour
 {
@@ -8,99 +9,142 @@ public class Player : MonoBehaviour
     public float speed;
     public int health;
     public float jumpForce;
+
+    [Header("Move Settings")]
+    Vector2 inputDirection;
+    float lookingDirection;
+    Vector2 moveDirection;
+
     [Header("Booleans")]
-    public bool isJumping;
-    public bool isAttacking;
+    bool isMoving;
+    bool isJumping;
+    bool isAttacking;
+
     [Header("Others")]
-    public Animator anim;
-    public Rigidbody2D rig;
-    public SpriteRenderer sprite;
+    Animator anim;
+    Rigidbody2D rig;
+    SpriteRenderer sprite;
 
+    [Header("SFX")]
+    [SerializeField] AudioClip jumpSFX;
+    [SerializeField] AudioClip attackSFX;
+    [SerializeField] AudioClip stepSFX;
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rig = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-    void Update()
+
+    private void Update()
     {
-        Jump();
-        playAttack();
+        InputListener();
+        GFXController();
     }
-    // Update is called once per frame
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
         Move();
-        
     }
 
+    void InputListener()
+    {
+        inputDirection.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDirection.Set(speed * inputDirection.x, rig.velocity.y);
+        lookingDirection = inputDirection.x != 0 ? inputDirection.x : lookingDirection;
+        isMoving = moveDirection.x != 0;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Attack();
+        }
+    }
+    
     void Move()
     {
-        
-        float inputX = Input.GetAxisRaw("Horizontal");
-
-        rig.velocity = new Vector2(inputX * speed, rig.velocity.y);
-
-        if (inputX > 0)
-        {
-            sprite.flipX = false;
-            if (!isJumping)
-            {
-                anim.SetInteger("transition", 1);
-            }
-        }
-        else if (inputX < 0)
-        {
-            sprite.flipX = true;
-            if (!isJumping)
-            {
-                anim.SetInteger("transition", 1);
-            }
-
-        }
-        if(inputX == 0 && !isJumping)
-        {
-            anim.SetInteger("transition", 0);
-        }
-        
+        rig.velocity = moveDirection;
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isJumping) return;
+        AudioSource.PlayClipAtPoint(jumpSFX, transform.position);
+        Vector2 jumpDirection = new Vector2(rig.velocity.x, jumpForce);
+        rig.velocity = jumpDirection;
+        isJumping = true;
+    }
+
+    void Attack()
+    {
+        if (isAttacking) return;
+        StartCoroutine(AttackExec());
+    }
+
+    void GFXController()
+    {
+        if (lookingDirection > 0)
         {
-            if (!isJumping)
-            {
-                rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-                isJumping = true;
-                anim.SetInteger("transition", 2);
-            }
-            
+            sprite.flipX = false;
+        }
+        if (lookingDirection < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        if (isAttacking)
+        {
+            anim.SetInteger("transition", 3);
+            return;
+        }
+
+        if (isJumping)
+        {
+            anim.SetInteger("transition", 2);
+            return;
+        }
+
+        if (isMoving)
+        {
+            anim.SetInteger("transition", 1);
+        }
+        else
+        {
+            anim.SetInteger("transition", 0);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    IEnumerator AttackExec()
     {
-        if (collision.collider.CompareTag("Ground"))
+        isAttacking = true;
+        float cacheSpeed = speed;
+        speed = 0;
+        AudioSource.PlayClipAtPoint(attackSFX, transform.position);
+
+
+        yield return new WaitForSeconds(0.6f);
+
+
+        speed = cacheSpeed;
+        isAttacking = false;
+    }
+
+    public void StepSound()
+    {
+        AudioSource.PlayClipAtPoint(stepSFX, transform.position);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!isJumping) return;
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        if (rig.velocity.y <= 0.1f)
         {
             isJumping = false;
-        }
-        
-    }
-    void playAttack()
-    {
-        StartCoroutine("Attack");
-    }
-    IEnumerator Attack()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            anim.SetInteger("transition", 3);
-            isAttacking = true;
-            yield return new WaitForSeconds(3f);
-            anim.SetInteger("transition", 0);
-            isAttacking = false;
         }
     }
 }
